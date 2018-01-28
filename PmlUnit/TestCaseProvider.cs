59446b2e955579b79024767e8e2806f09d142524
Florian Zimmermann <flo@chaos-wg.net>
2018-01-28 15:00:14 +0100

    add an EnvironmentVariableTestCaseProvider which loads test cases from the pml.index files in the paths referenced by the PMLLIB environment variable

--- PmlUnit/TestCaseProvider.cs
+++ PmlUnit/TestCaseProvider.cs
@@ -10,6 +10,57 @@ namespace PmlUnit
         ICollection<TestCase> GetTestCases();
     }
 
+    class EnvironmentVariableTestCaseProvider : TestCaseProvider
+    {
+        private readonly string[] Paths;
+
+        public EnvironmentVariableTestCaseProvider()
+            : this("PMLLIB")
+        {
+        }
+
+        public EnvironmentVariableTestCaseProvider(string variableName)
+        {
+            if (string.IsNullOrEmpty(variableName))
+                throw new ArgumentNullException(nameof(variableName));
+
+            var pmllib = Environment.GetEnvironmentVariable(variableName);
+            if (string.IsNullOrEmpty(pmllib))
+                Paths = new string[0];
+            else if (pmllib.IndexOf(Path.PathSeparator) >= 0)
+                Paths = pmllib.Split(Path.PathSeparator);
+            else if (Directory.Exists(pmllib))
+                Paths = new string[] { pmllib };
+            else
+                Paths = pmllib.Split(' ');
+        }
+
+        public ICollection<TestCase> GetTestCases()
+        {
+            var result = new List<TestCase>();
+
+            foreach (string path in Paths)
+            {
+                if (!Path.IsPathRooted(path))
+                    continue;
+                
+                TestCaseProvider provider;
+                try
+                {
+                    provider = new IndexFileTestCaseProvider(Path.Combine(path, "pml.index"));
+                }
+                catch (FileNotFoundException)
+                {
+                    continue;
+                }
+
+                result.AddRange(provider.GetTestCases());
+            }
+
+            return result;
+        }
+    }
+
     class IndexFileTestCaseProvider : TestCaseProvider
     {
         private readonly TestCaseParser Parser;
