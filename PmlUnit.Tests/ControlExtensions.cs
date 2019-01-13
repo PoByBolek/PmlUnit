56e86426143c73b7a17a4aa346891bc84be2ff83
Florian Zimmermann <flo@chaos-wg.net>
2019-01-13 21:45:35 +0100

    always use NUnit to make assertions (instead of the Visual Studio test framework)

--- PmlUnit.Tests/ControlExtensions.cs
+++ PmlUnit.Tests/ControlExtensions.cs
@@ -4,7 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NUnit.Framework;
 
 namespace PmlUnit.Tests
 {
