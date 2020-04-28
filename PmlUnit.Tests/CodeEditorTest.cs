e1ccaed961b8d2efac633b457d2c3823e1561584
Florian Zimmermann <flo@chaos-wg.net>
2020-04-28 17:04:42 +0200

    add CodeEditor implementations that manage opening files with a specific code editor

---
+++ PmlUnit.Tests/CodeEditorTest.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) 2020 Florian Zimmermann.
+// Licensed under the MIT License: https://opensource.org/licenses/MIT
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace PmlUnit.Tests
+{
+    [TestFixture]
+    [TestOf(typeof(CodeEditorDescriptor))]
+    public class CodeEditorDescriptorTest
+    {
+        public void ValidatesNullArguments()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CodeEditorDescriptor(CodeEditorKind.Other, null, null));
+            Assert.Throws<ArgumentNullException>(() => new CodeEditorDescriptor(CodeEditorKind.Other, null, ""));
+            Assert.Throws<ArgumentNullException>(() => new CodeEditorDescriptor(CodeEditorKind.Other, "", ""));
+            Assert.Throws<ArgumentNullException>(() => new CodeEditorDescriptor(CodeEditorKind.Other, "", null));
+            Assert.Throws<ArgumentNullException>(() => new CodeEditorDescriptor(CodeEditorKind.Other, "foo", null));
+
+            var editor = new CodeEditorDescriptor(CodeEditorKind.Other, "foo", "");
+            Assert.Throws<ArgumentNullException>(() => editor.FileName = null);
+            Assert.Throws<ArgumentNullException>(() => editor.FileName = "");
+            Assert.Throws<ArgumentNullException>(() => editor.FixedArguments = null);
+            editor.FixedArguments = "";
+        }
+        
+        [TestCase(CodeEditorKind.Atom, @"C:\Program Files\Atom\atom.exe", "random stuff", typeof(AtomCodeEditor))]
+        [TestCase(CodeEditorKind.Atom, @"C:\Users\Current\AppData\Local\Atom\atom.exe", "other stuff", typeof(AtomCodeEditor))]
+        [TestCase(CodeEditorKind.NotepadPlusPlus, @"C:\Program Files\Notepad++\notepad++.exe", "foobar", typeof(NotepadPlusPlusCodeEditor))]
+        [TestCase(CodeEditorKind.NotepadPlusPlus, @"C:\Program Files (x86)\Notepad++\notepad++.exe", "foobar", typeof(NotepadPlusPlusCodeEditor))]
+        [TestCase(CodeEditorKind.SublimeText, @"C:\Program Files\Sublime Text 3\subl.exe", "", typeof(SublimeTextCodeEditor))]
+        [TestCase(CodeEditorKind.SublimeText, @"C:\Program Files\Sublime Text 3\subl.exe", "empty", typeof(SublimeTextCodeEditor))]
+        [TestCase(CodeEditorKind.UltraEdit, @"C:\Program Files\UltraEdit\uedit64.exe", "--test", typeof(UltraEditCodeEditor))]
+        [TestCase(CodeEditorKind.UltraEdit, @"C:\Program Files (x86)\UltraEdit\uedit32.exe", "--clear", typeof(UltraEditCodeEditor))]
+        [TestCase(CodeEditorKind.VisualStudioCode, @"C:\Users\Current\AppData\Local\Programs\Microsoft VS Code\Code.exe", "stuff", typeof(VisualStudioCodeCodeEditor))]
+        [TestCase(CodeEditorKind.VisualStudioCode, @"C:\Program Files\Microsoft VS Code\Code.exe", "random", typeof(VisualStudioCodeCodeEditor))]
+        [TestCase(CodeEditorKind.Other, @"C:\Program Files\Editor\Editor.exe", "--be-awesome", typeof(OtherCodeEditor))]
+        [TestCase(CodeEditorKind.Other, @"C:\Program Files\Something Else\foo.exe", "--bar", typeof(OtherCodeEditor))]
+        public void CreatesCodeEditorInstance(int kind, string fileName, string fixedArguments, Type expectedType)
+        {
+            var descriptor = new CodeEditorDescriptor((CodeEditorKind)kind, fileName, fixedArguments);
+            var editor = descriptor.ToEditor();
+            Assert.That(editor, Is.InstanceOf(expectedType));
+            var fileNameField = expectedType.GetField("FileName", BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.That(fileNameField.GetValue(editor), Is.EqualTo(fileName));
+            var fixedArgumentsField = expectedType.GetField("FixedArguments", BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.That(fixedArgumentsField.GetValue(editor), Is.EqualTo(fixedArguments));
+        }
+    }
+
+    [TestFixture]
+    [TestOf(typeof(BaseCodeEditor))]
+    public class CodeEditorTest
+    {
+        [TestCase(@"C:\path\to\file.pmlobj:25", "", @"C:\path\to\file.pmlobj:25")]
+        [TestCase("\"C:\\path with spaces\\to\\file.pmlfrm:26\"", "", @"C:\path with spaces\to\file.pmlfrm:26")]
+        [TestCase("multiple arguments \"that may have\" spaces", "", "multiple", "arguments", "that may have", "spaces")]
+        [TestCase("\"quotes\\\"in\\\"arguments\\\"must\\\"be\\\"escaped\"", "", "quotes\"in\"arguments\"must\"be\"escaped")]
+        [TestCase("backslashes\\must only be escaped \"before\\\\\\\"quotes\"", "", "backslashes\\must", "only", "be", "escaped", "before\\\"quotes")]
+        [TestCase("fixed arguments should\"be copied untouched \"dynamic arguments should come after the fixed ones\"", "fixed arguments should\"be copied untouched", "dynamic arguments should come after the fixed ones")]
+        public void EscapesCommandLineArguments(string expectedArguments, string fixedArguments, params string[] extraArguments)
+        {
+            var editor = new StubCodeEditor(@"C:\Program Files\Editor\editor.exe", fixedArguments, extraArguments);
+            Assert.That(editor.Arguments, Is.EqualTo(expectedArguments));
+        }
+
+        [TestCase(typeof(AtomCodeEditor), @"C:\path\to\file.pmlobj", 12345, @"C:\path\to\file.pmlobj:12345")]
+        [TestCase(typeof(AtomCodeEditor), @"C:\path\to\file.pmlobj", 0, @"C:\path\to\file.pmlobj")]
+        [TestCase(typeof(AtomCodeEditor), @"C:\path to\file.pmlobj", 1234, "\"C:\\path to\\file.pmlobj:1234\"")]
+        [TestCase(typeof(AtomCodeEditor), @"C:\path to\file.pmlobj", 0, "\"C:\\path to\\file.pmlobj\"")]
+        [TestCase(typeof(NotepadPlusPlusCodeEditor), @"C:\path\to\file.pmlobj", 12345, @"-n12345 C:\path\to\file.pmlobj")]
+        [TestCase(typeof(NotepadPlusPlusCodeEditor), @"C:\path\to\file.pmlobj", 0, @"C:\path\to\file.pmlobj")]
+        [TestCase(typeof(NotepadPlusPlusCodeEditor), @"C:\path to\file.pmlobj", 1234, "-n1234 \"C:\\path to\\file.pmlobj\"")]
+        [TestCase(typeof(NotepadPlusPlusCodeEditor), @"C:\path to\file.pmlobj", 0, "\"C:\\path to\\file.pmlobj\"")]
+        [TestCase(typeof(SublimeTextCodeEditor), @"C:\path\to\file.pmlobj", 12345, @"C:\path\to\file.pmlobj:12345")]
+        [TestCase(typeof(SublimeTextCodeEditor), @"C:\path\to\file.pmlobj", 0, @"C:\path\to\file.pmlobj")]
+        [TestCase(typeof(SublimeTextCodeEditor), @"C:\path to\file.pmlobj", 1234, "\"C:\\path to\\file.pmlobj:1234\"")]
+        [TestCase(typeof(SublimeTextCodeEditor), @"C:\path to\file.pmlobj", 0, "\"C:\\path to\\file.pmlobj\"")]
+        [TestCase(typeof(UltraEditCodeEditor), @"C:\path\to\file.pmlobj", 12345, @"C:\path\to\file.pmlobj -l12345")]
+        [TestCase(typeof(UltraEditCodeEditor), @"C:\path\to\file.pmlobj", 0, @"C:\path\to\file.pmlobj")]
+        [TestCase(typeof(UltraEditCodeEditor), @"C:\path to\file.pmlobj", 1234, "\"C:\\path to\\file.pmlobj\" -l1234")]
+        [TestCase(typeof(UltraEditCodeEditor), @"C:\path to\file.pmlobj", 0, "\"C:\\path to\\file.pmlobj\"")]
+        [TestCase(typeof(VisualStudioCodeCodeEditor), @"C:\path\to\file.pmlobj", 12345, @"--goto C:\path\to\file.pmlobj:12345")]
+        [TestCase(typeof(VisualStudioCodeCodeEditor), @"C:\path\to\file.pmlobj", 0, @"C:\path\to\file.pmlobj")]
+        [TestCase(typeof(VisualStudioCodeCodeEditor), @"C:\path to\file.pmlobj", 1234, "--goto \"C:\\path to\\file.pmlobj:1234\"")]
+        [TestCase(typeof(VisualStudioCodeCodeEditor), @"C:\path to\file.pmlobj", 0, "\"C:\\path to\\file.pmlobj\"")]
+        public void BuildsProcessStartInfoCorrectly(Type editorType, string fileName, int lineNumber, string expectedArguments)
+        {
+            var constructor = editorType.GetConstructor(new Type[] { typeof(string), typeof(string) });
+            var editor = constructor.Invoke(new object[] { @"C:\Program Files\Editor\Editor.exe", "" });
+            var method = editorType.GetMethod("BuildStartInfo", BindingFlags.Instance | BindingFlags.NonPublic);
+            var startInfo = method.Invoke(editor, new object[] { fileName, lineNumber }) as ProcessStartInfo;
+            Assert.That(startInfo.FileName, Is.EqualTo(@"C:\Program Files\Editor\Editor.exe"));
+            Assert.That(startInfo.Arguments, Is.EqualTo(expectedArguments));
+            Assert.That(startInfo.UseShellExecute, Is.False);
+        }
+    }
+
+    class StubCodeEditor : BaseCodeEditor
+    {
+        private readonly IEnumerable<string> ExtraArguments;
+
+        public StubCodeEditor(string fileName, string fixedArguments, IEnumerable<string> extraArguments)
+            : base(fileName, fixedArguments)
+        {
+            ExtraArguments = extraArguments.ToList();
+        }
+
+        public string Arguments
+        {
+            get { return BuildStartInfo("foo", 1).Arguments; }
+        }
+
+        protected override IEnumerable<string> GetExtraArguments(string fileName, int lineNumber)
+        {
+            return ExtraArguments;
+        }
+    }
+}
---
+++ PmlUnit/CodeEditor.cs
@@ -0,0 +1,258 @@
+﻿// Copyright (c) 2020 Florian Zimmermann.
+// Licensed under the MIT License: https://opensource.org/licenses/MIT
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+namespace PmlUnit
+{
+    interface CodeEditor
+    {
+        void OpenFile(string fileName, int lineNumber);
+    }
+
+    sealed class CodeEditorDescriptor
+    {
+        public CodeEditorKind Kind { get; set; }
+
+        private string FileNameField;
+        private string FixedArgumentsField;
+
+        public CodeEditorDescriptor(CodeEditorKind kind, string fileName, string arguments)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+            if (arguments == null)
+                throw new ArgumentNullException(nameof(arguments));
+
+            Kind = kind;
+            FileNameField = fileName;
+            FixedArgumentsField = arguments;
+        }
+
+        public string FileName
+        {
+            get { return FileNameField; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException(nameof(value));
+                FileNameField = value;
+            }
+        }
+
+        public string FixedArguments
+        {
+            get { return FixedArgumentsField; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+                FixedArgumentsField = value;
+            }
+        }
+
+        public CodeEditor ToEditor()
+        {
+            switch (Kind)
+            {
+                case CodeEditorKind.Atom:
+                    return new AtomCodeEditor(FileNameField, FixedArgumentsField);
+                case CodeEditorKind.NotepadPlusPlus:
+                    return new NotepadPlusPlusCodeEditor(FileNameField, FixedArgumentsField);
+                case CodeEditorKind.SublimeText:
+                    return new SublimeTextCodeEditor(FileNameField, FixedArgumentsField);
+                case CodeEditorKind.UltraEdit:
+                    return new UltraEditCodeEditor(FileNameField, FixedArgumentsField);
+                case CodeEditorKind.VisualStudioCode:
+                    return new VisualStudioCodeCodeEditor(FileNameField, FixedArgumentsField);
+                default:
+                    return new OtherCodeEditor(FileNameField, FixedArgumentsField);
+            }
+        }
+    }
+
+    enum CodeEditorKind
+    {
+        Other = 0,
+        Atom = 0x41544f4d, // ATOM
+        NotepadPlusPlus = 0x4e502b2b, // NP++
+        SublimeText = 0x5355424c, // SUBL
+        UltraEdit = 0x55454454, // UEDT
+        VisualStudioCode = 0x56534344, // VSCD
+    }
+
+    abstract class BaseCodeEditor : CodeEditor
+    {
+        protected readonly string FileName;
+        protected readonly string FixedArguments;
+
+        protected BaseCodeEditor(string fileName, string fixedArguments)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+
+            FileName = fileName;
+            FixedArguments = fixedArguments ?? "";
+        }
+
+        public void OpenFile(string fileName, int lineNumber)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+            if (lineNumber < 0)
+                throw new ArgumentOutOfRangeException(nameof(lineNumber));
+
+            Process.Start(BuildStartInfo(fileName, lineNumber));
+        }
+
+        protected ProcessStartInfo BuildStartInfo(string fileName, int lineNumber)
+        {
+            var result = new ProcessStartInfo(FileName);
+            result.UseShellExecute = false;
+
+            var arguments = new StringBuilder(FixedArguments);
+            foreach (string argument in GetExtraArguments(fileName, lineNumber))
+                arguments.Append(' ').Append(Escape(argument));
+            result.Arguments = arguments.ToString().Trim();
+
+            return result;
+        }
+
+        private static string Escape(string argument)
+        {
+            // see https://docs.microsoft.com/en-us/previous-versions/17w5ykft(v=vs.85) << note the closing parenthesis
+            int index = argument.IndexOfAny(new char[] { ' ', '\t', '"' });
+            if (index < 0)
+                return argument;
+
+            var result = new StringBuilder();
+            int backslashes = 0;
+            result.Append('"');
+            foreach (char c in argument)
+            {
+                if (c == '\\')
+                {
+                    backslashes++;
+                }
+                else if (c == '"')
+                {
+                    // escape all backslashes before this quote and the quote itself
+                    result.Append('\\', 2 * backslashes + 1).Append('"');
+                    backslashes = 0;
+                }
+                else
+                {
+                    // backslashes only have a special meaning before quotes
+                    result.Append('\\', backslashes).Append(c);
+                    backslashes = 0;
+                }
+            }
+            // escape all backslashes before the final quote so that it does not get escaped
+            result.Append('\\', 2 * backslashes).Append('"');
+            return result.ToString();
+        }
+
+        protected abstract IEnumerable<string> GetExtraArguments(string fileName, int lineNumber);
+    }
+
+    class AtomCodeEditor : BaseCodeEditor
+    {
+        public AtomCodeEditor(string fileName, string arguments)
+            : base(fileName, arguments)
+        {
+        }
+
+        protected override IEnumerable<string> GetExtraArguments(string fileName, int lineNumber)
+        {
+            // see https://flight-manual.atom.io/getting-started/sections/atom-basics/
+            if (lineNumber > 0)
+                return new string[] { fileName + ":" + lineNumber.ToString(CultureInfo.InvariantCulture) };
+            else
+                return new string[] { fileName };
+        }
+    }
+
+    class NotepadPlusPlusCodeEditor : BaseCodeEditor
+    {
+        public NotepadPlusPlusCodeEditor(string fileName, string arguments)
+            : base(fileName, arguments)
+        {
+        }
+
+        protected override IEnumerable<string> GetExtraArguments(string fileName, int lineNumber)
+        {
+            // see https://npp-user-manual.org/docs/command-prompt/
+            if (lineNumber > 0)
+                return new string[] { "-n" + lineNumber.ToString(CultureInfo.InvariantCulture), fileName };
+            else
+                return new string[] { fileName };
+        }
+    }
+
+    class SublimeTextCodeEditor : BaseCodeEditor
+    {
+        public SublimeTextCodeEditor(string fileName, string arguments)
+            : base(fileName, arguments)
+        {
+        }
+
+        protected override IEnumerable<string> GetExtraArguments(string fileName, int lineNumber)
+        {
+            // see https://sublime-text-unofficial-documentation.readthedocs.io/en/latest/command_line/command_line.html
+            if (lineNumber > 0)
+                return new string[] { fileName + ":" + lineNumber.ToString(CultureInfo.InvariantCulture) };
+            else
+                return new string[] { fileName };
+        }
+    }
+
+    class UltraEditCodeEditor : BaseCodeEditor
+    {
+        public UltraEditCodeEditor(string fileName, string arguments)
+            : base(fileName, arguments)
+        {
+        }
+
+        protected override IEnumerable<string> GetExtraArguments(string fileName, int lineNumber)
+        {
+            // see https://www.ultraedit.com/wiki/Command_line_parameters
+            if (lineNumber > 0)
+                return new string[] { fileName, "-l" + lineNumber.ToString(CultureInfo.InvariantCulture) };
+            else
+                return new string[] { fileName };
+        }
+    }
+
+    class VisualStudioCodeCodeEditor : BaseCodeEditor
+    {
+        public VisualStudioCodeCodeEditor(string fileName, string arguments)
+            : base(fileName, arguments)
+        {
+        }
+
+        protected override IEnumerable<string> GetExtraArguments(string fileName, int lineNumber)
+        {
+            // see https://code.visualstudio.com/docs/editor/command-line
+            if (lineNumber > 0)
+                return new string[] { "--goto", fileName + ":" + lineNumber.ToString(CultureInfo.InvariantCulture) };
+            else
+                return new string[] { fileName };
+        }
+    }
+
+    class OtherCodeEditor : BaseCodeEditor
+    {
+        public OtherCodeEditor(string fileName, string arguments)
+            : base(fileName, arguments)
+        {
+        }
+
+        protected override IEnumerable<string> GetExtraArguments(string fileName, int lineNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
