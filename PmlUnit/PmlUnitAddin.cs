fb1c09e33a877d0ff2efa98b2a8fa8ca55e6e1e2
Florian Zimmermann <flo@chaos-wg.net>
2020-05-29 14:37:17 +0200

    explicitly set the font of TestRunnerControl and AboutDialog when starting PML Unit
    Since we are only displaying English text, we don't have to rely on Control.DefaultFont to select an appropriate font for us.
    This partially fixes issue #2.

--- PmlUnit/PmlUnitAddin.cs
+++ PmlUnit/PmlUnitAddin.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2019 Florian Zimmermann.
 // Licensed under the MIT License: https://opensource.org/licenses/MIT
 using System;
+using System.Drawing;
+using System.Drawing.Text;
 using Aveva.ApplicationFramework;
 
 #if PDMS || E3D_11
@@ -24,8 +26,11 @@ namespace PmlUnit
 
         public PmlUnitAddin()
         {
+            Font font = null;
             try
             {
+                font = GetDefaultFont();
+
                 var index = new FileIndex();
                 TestRunner = new PmlTestRunner(
                     new ControlMethodInvoker(() => TestRunnerControl),
@@ -33,10 +38,14 @@ namespace PmlUnit
                 );
                 TestCaseProvider = new FileIndexTestCaseProvider(index);
                 TestRunnerControl = new TestRunnerControl(TestCaseProvider, TestRunner);
+                TestRunnerControl.Font = font;
                 AboutDialog = new AboutDialog();
+                AboutDialog.Font = font;
             }
             catch
             {
+                if (font != null)
+                    font.Dispose();
                 if (TestRunner != null)
                     TestRunner.Dispose();
                 if (TestRunnerControl != null)
@@ -54,15 +63,21 @@ namespace PmlUnit
             if (runner == null)
                 throw new ArgumentNullException(nameof(runner));
 
+            Font font = null;
             TestCaseProvider = provider;
             TestRunner = runner;
             try
             {
+                font = GetDefaultFont();
                 TestRunnerControl = new TestRunnerControl(TestCaseProvider, TestRunner);
+                TestRunnerControl.Font = font;
                 AboutDialog = new AboutDialog();
+                AboutDialog.Font = font;
             }
             catch
             {
+                if (font != null)
+                    font.Dispose();
                 if (TestRunnerControl != null)
                     TestRunnerControl.Dispose();
                 if (AboutDialog != null)
@@ -142,6 +157,82 @@ namespace PmlUnit
             // PDMS crashes if we also dispose the TestRunnerControl here
             TestRunner.Dispose();
             AboutDialog.Dispose();
+        }
+
+        /// <summary>
+        /// Gets the default font that we will be using for all controls and windows in PML Unit.
+        /// <para>
+        /// This is necessary because on Windows versions that don't use Latin script
+        /// <code>Control.DefaultFont</code> may look weird with our Controls only displaying
+        /// English text.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// TOOD: remove this when PML Unit is localized for languages with non-Latin script
+        /// and specify the font families and sizes in resource files.
+        /// 
+        /// See https://stackoverflow.com/questions/61801155/default-font-for-windows-forms-control-with-latin-script-on-a-chinese-windows
+        /// </remarks>
+        private static Font GetDefaultFont()
+        {
+            // Although the menus and ribbons of PDMS and E3D use SystemFonts.MenuFont
+            // (and even respond to font changes in the system settings), almost all
+            // other forms and controls (especially PML forms) use a fixed font which
+            // almost certainly is Microsoft Sans Serif. So we try that one first to
+            // better match the overall look and feel.
+            Font result;
+            if (TryGetFont("Microsoft Sans Serif", 8.25f, out result))
+                return result;
+            else if (TryGetFont("Segoe UI", 9f, out result))
+                return result;
+            else if (TryGetFont("Arial", 8.25f, out result))
+                return result;
+            else if (TryGetFont(GenericFontFamilies.SansSerif, 9f, out result))
+                return result;
+            else
+                // clone the system font because our callers may dispose it
+                return (Font)SystemFonts.MenuFont.Clone();
+        }
+
+        private static bool TryGetFont(string familyName, float size, out Font result)
+        {
+            FontFamily family = null;
+            try
+            {
+                family = new FontFamily(familyName);
+            }
+            catch (ArgumentException)
+            {
+                result = null;
+                return false;
+            }
+
+            using (family)
+            {
+                return TryGetFont(family, size, out result);
+            }
+        }
+
+        private static bool TryGetFont(GenericFontFamilies genericFamily, float size, out Font result)
+        {
+            using (var family = new FontFamily(genericFamily))
+            {
+                return TryGetFont(family, size, out result);
+            }
+        }
+
+        private static bool TryGetFont(FontFamily family, float size, out Font result)
+        {
+            try
+            {
+                result = new Font(family, size);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                result = null;
+                return false;
+            }
         }
     }
 }
